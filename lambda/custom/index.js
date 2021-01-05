0df5aa85e2068ad2eabf772d607b198fbc746a5b
Kishan Kolur <kishankolur@Kishans-MacBook-Pro.local>
2021-01-05 11:17:09 -0500

    add fizzbuzz intent logic and update other default intents

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -13,157 +13,97 @@ import {
   getSlotValue,
   SkillBuilders,
 } from "ask-sdk-core";
-import { getLaunchMessage, getNewGameMessage, getGameOverMessage, getRepromptMessage, getErrorMessage, getExitMessage } from "./messages";
+import {
+  getLaunchMessage,
+  getStartMessage,
+  getGameOverMessage,
+  getGameOverRepromptMessage,
+  getRepromptMessage,
+  getErrorMessage,
+  getExitMessage,
+} from "./messages";
+
+const STATES = {
+  STARTING: "STARTED",
+  ENDED: "ENDED",
+};
+const FIZZ_NUMBER = 3;
+const BUZZ_NUMBER = 5;
+const FIZZBUZZ_SLOT = "FizzBuzz";
+const NUMBER_SLOT = "Number";
+
+function fizzBuzzHelper(number) {
+  if (number % FIZZ_NUMBER === 0 && number % BUZZ_NUMBER === 0) {
+    return "fizzbuzz";
+  } else if (number % FIZZ_NUMBER === 0) {
+    return "fizz";
+  } else if (number % BUZZ_NUMBER === 0) {
+    return "buzz";
+  } else {
+    return number.toString();
+  }
+}
+
+function getExpectedNumberAsString(number) {
+  return fizzBuzzHelper(number + 1);
+}
 
 const LaunchRequest = {
   canHandle(handlerInput) {
-    // launch requests as well as any new session, as games are not saved in progress, which makes
+    // launch requests as well sas any new session, as games are not saved in progress, which makes
     // no one shots a reasonable idea except for help, and the welcome message provides some help.
     return (
       isNewSession(handlerInput.requestEnvelope) ||
-      getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+      getRequestType(handlerInput.requestEnvelope) === "LaunchRequest" ||
+      getRequestType(handlerInput.requestEnvelope) === "GameIntent" ||
+      getRequestType(handlerInput.requestEnvelope) === "AMAZON.StartOverIntent"
     );
   },
   async handle(handlerInput) {
     const { attributesManager } = handlerInput;
-    const attributes =
-      (await attributesManager.getPersistentAttributes()) || {};
-
-    if (Object.keys(attributes).length === 0) {
-      attributes.endedSessionCount = 0;
-      attributes.gameState = "ENDED";
-    }
+    const attributes = attributesManager.getSessionAttributes();
+    attributes.gameState = STATES.STARTING;
+    attributes.currentNumber = 1;
 
     attributesManager.setSessionAttributes(attributes);
 
-    const speechOutput = getLaunchMessage;
-
     return handlerInput.responseBuilder
-      .speak(speechOutput)
-      .reprompt(speechOutput)
+      .speak(getLaunchMessage + getStartMessage)
+      .reprompt(getStartMessage)
       .getResponse();
   },
 };
 
-// const ExitHandler = {
-//   canHandle(handlerInput) {
-//     return (
-//       getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-//       (getIntentName(handlerInput.requestEnvelope) === "AMAZON.CancelIntent" ||
-//         getIntentName(handlerInput.requestEnvelope) === "AMAZON.StopIntent")
-//     );
-//   },
-//   handle(handlerInput) {
-//     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+const ExitHandler = {
+  canHandle(handlerInput) {
+    return (
+      getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      (getIntentName(handlerInput.requestEnvelope) === "AMAZON.CancelIntent" ||
+        getIntentName(handlerInput.requestEnvelope) === "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder.speak(getExitMessage).getResponse();
+  },
+};
 
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("EXIT_MESSAGE"))
-//       .getResponse();
-//   },
-// };
+const HelpIntent = {
+  canHandle(handlerInput) {
+    return (
+      getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
 
-// const SessionEndedRequest = {
-//   canHandle(handlerInput) {
-//     return (
-//       getRequestType(handlerInput.requestEnvelope) === "SessionEndedRequest"
-//     );
-//   },
-//   handle(handlerInput) {
-//     console.log(
-//       `Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`
-//     );
-//     return handlerInput.responseBuilder.getResponse();
-//   },
-// };
-
-// const HelpIntent = {
-//   canHandle(handlerInput) {
-//     return (
-//       getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-//       getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
-//     );
-//   },
-//   handle(handlerInput) {
-//     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("HELP_MESSAGE"))
-//       .reprompt(requestAttributes.t("HELP_REPROMPT"))
-//       .getResponse();
-//   },
-// };
-
-// const YesIntent = {
-//   canHandle(handlerInput) {
-//     // only start a new game if yes is said when not playing a game.
-//     let isCurrentlyPlaying = false;
-//     const { attributesManager } = handlerInput;
-//     const sessionAttributes = attributesManager.getSessionAttributes();
-
-//     if (
-//       sessionAttributes.gameState &&
-//       sessionAttributes.gameState === "STARTED"
-//     ) {
-//       isCurrentlyPlaying = true;
-//     }
-
-//     return (
-//       !isCurrentlyPlaying &&
-//       getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-//       getIntentName(handlerInput.requestEnvelope) === "AMAZON.YesIntent"
-//     );
-//   },
-//   handle(handlerInput) {
-//     const { attributesManager } = handlerInput;
-//     const requestAttributes = attributesManager.getRequestAttributes();
-//     const sessionAttributes = attributesManager.getSessionAttributes();
-
-//     sessionAttributes.gameState = "STARTED";
-//     sessionAttributes.guessNumber = Math.floor(Math.random() * 101);
-
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("YES_MESSAGE"))
-//       .reprompt(requestAttributes.t("HELP_REPROMPT"))
-//       .getResponse();
-//   },
-// };
-
-// const NoIntent = {
-//   canHandle(handlerInput) {
-//     // only treat no as an exit when outside a game
-//     let isCurrentlyPlaying = false;
-//     const { attributesManager } = handlerInput;
-//     const sessionAttributes = attributesManager.getSessionAttributes();
-
-//     if (
-//       sessionAttributes.gameState &&
-//       sessionAttributes.gameState === "STARTED"
-//     ) {
-//       isCurrentlyPlaying = true;
-//     }
-
-//     return (
-//       !isCurrentlyPlaying &&
-//       getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-//       getIntentName(handlerInput.requestEnvelope) === "AMAZON.NoIntent"
-//     );
-//   },
-//   async handle(handlerInput) {
-//     const { attributesManager } = handlerInput;
-//     const requestAttributes = attributesManager.getRequestAttributes();
-//     const sessionAttributes = attributesManager.getSessionAttributes();
-
-//     sessionAttributes.endedSessionCount += 1;
-//     sessionAttributes.gameState = "ENDED";
-//     attributesManager.setPersistentAttributes(sessionAttributes);
-
-//     await attributesManager.savePersistentAttributes();
-
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("EXIT_MESSAGE"))
-//       .getResponse();
-//   },
-// };
+    return handlerInput.responseBuilder
+      .speak(getLaunchMessage)
+      .reprompt(getRepromptMessage(sessionAttributes.currentNumber))
+      .getResponse();
+  },
+};
 
 // const UnhandledIntent = {
 //   canHandle() {
@@ -179,81 +119,86 @@ const LaunchRequest = {
 //   },
 // };
 
-// const NumberGuessIntent = {
-//   canHandle(handlerInput) {
-//     // handle numbers only during a game
-//     let isCurrentlyPlaying = false;
-//     const { attributesManager } = handlerInput;
-//     const sessionAttributes = attributesManager.getSessionAttributes();
+const FizzBuzzIntent = {
+  canHandle(handlerInput) {
+    // handle numbers only during a game
+    let isCurrentlyPlaying = false;
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
 
-//     if (
-//       sessionAttributes.gameState &&
-//       sessionAttributes.gameState === "STARTED"
-//     ) {
-//       isCurrentlyPlaying = true;
-//     }
+    if (
+      sessionAttributes.gameState &&
+      sessionAttributes.gameState === "STARTED"
+    ) {
+      isCurrentlyPlaying = true;
+    }
 
-//     return (
-//       isCurrentlyPlaying &&
-//       getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-//       getIntentName(handlerInput.requestEnvelope) === "NumberGuessIntent"
-//     );
-//   },
-//   async handle(handlerInput) {
-//     const { attributesManager } = handlerInput;
-//     const requestAttributes = attributesManager.getRequestAttributes();
-//     const sessionAttributes = attributesManager.getSessionAttributes();
+    return (
+      isCurrentlyPlaying &&
+      getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      getIntentName(handlerInput.requestEnvelope) === "FizzBuzzIntent"
+    );
+  },
+  async handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
 
-//     const guessNum = parseInt(
-//       getSlotValue(handlerInput.requestEnvelope, "number"),
-//       10
-//     );
-//     const targetNum = sessionAttributes.guessNumber;
+    const answer =
+      getSlotValue(handlerInput.requestEnvelope, FIZZBUZZ_SLOT) === null
+        ? getSlotValue(handlerInput.requestEnvelope, NUMBER_SLOT)
+        : getSlotValue(handlerInput.requestEnvelope, FIZZBUZZ_SLOT);
+    const expected = getExpectedNumberAsString(sessionAttributes.currentNumber);
 
-//     if (guessNum > targetNum) {
-//       return handlerInput.responseBuilder
-//         .speak(requestAttributes.t("TOO_HIGH_MESSAGE", guessNum.toString()))
-//         .reprompt(requestAttributes.t("TOO_HIGH_REPROMPT"))
-//         .getResponse();
-//     } else if (guessNum < targetNum) {
-//       return handlerInput.responseBuilder
-//         .speak(requestAttributes.t("TOO_LOW_MESSAGE", guessNum.toString()))
-//         .reprompt(requestAttributes.t("TOO_LOW_REPROMPT"))
-//         .getResponse();
-//     } else if (guessNum === targetNum) {
-//       sessionAttributes.gamesPlayed += 1;
-//       sessionAttributes.gameState = "ENDED";
-//       attributesManager.setPersistentAttributes(sessionAttributes);
-//       await attributesManager.savePersistentAttributes();
-//       return handlerInput.responseBuilder
-//         .speak(
-//           requestAttributes.t("GUESS_CORRECT_MESSAGE", guessNum.toString())
-//         )
-//         .reprompt(requestAttributes.t("CONTINUE_MESSAGE"))
-//         .getResponse();
-//     }
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("FALLBACK_MESSAGE_DURING_GAME"))
-//       .reprompt(requestAttributes.t("FALLBACK_REPROMPT_DURING_GAME"))
-//       .getResponse();
-//   },
-// };
+    if (!answer) {
+      return handlerInput.responseBuilder
+        .speak(getErrorMessage(sessionAttributes.currentNumber))
+        .reprompt(getRepromptMessage(sessionAttributes.currentNumber))
+        .getResponse();
+    }
 
-// const ErrorHandler = {
-//   canHandle() {
-//     return true;
-//   },
-//   handle(handlerInput, error) {
-//     console.log(`Error handled: ${error.message}`);
-//     console.log(`Error stack: ${error.stack}`);
-//     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    if (expected === answer) {
+      // Correct Answer
+      sessionAttributes.gameState = STATES.STARTING;
 
-//     return handlerInput.responseBuilder
-//       .speak(requestAttributes.t("ERROR_MESSAGE"))
-//       .reprompt(requestAttributes.t("ERROR_MESSAGE"))
-//       .getResponse();
-//   },
-// };
+      // handle increment because of Alexa saying the next number
+      sessionAttributes.currentNumber += 2;
+      attributesManager.setSessionAttributes(sessionAttributes);
+
+      const newNumberAsString = fizzBuzzHelper(sessionAttributes.currentNumber);
+
+      return handlerInput.responseBuilder
+        .speak(newNumberAsString)
+        .reprompt(getRepromptMessage(newNumberAsString))
+        .getResponse();
+    } else {
+      // Incorrect Answer - game over
+      sessionAttributes.gameState = STATES.ENDED;
+      attributesManager.setSessionAttributes(sessionAttributes);
+      return handlerInput.responseBuilder
+        .speak(getGameOverMessage(expected))
+        .reprompt(getGameOverRepromptMessage)
+        .getResponse();
+    }
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    return handlerInput.responseBuilder
+      .speak(getErrorMessage(sessionAttributes.currentNumber))
+      .reprompt(getErrorMessage(sessionAttributes.currentNumber))
+      .getResponse();
+  },
+};
 
 // const FallbackHandler = {
 //   canHandle(handlerInput) {
@@ -298,13 +243,10 @@ export const handler = skillBuilder
   .addRequestHandlers(
     LaunchRequest,
     ExitHandler,
-    SessionEndedRequest,
     HelpIntent,
-    YesIntent,
-    NoIntent,
-    NumberGuessIntent,
-    FallbackHandler,
-    UnhandledIntent
+    FizzBuzzIntent
+    // FallbackHandler,
+    // UnhandledIntent
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
--- lambda/custom/messages.js
+++ lambda/custom/messages.js
@@ -1,9 +1,10 @@
-export const getNewGameMessage = `OK. I'll start... One. `;
+export const getStartMessage = `OK. I'll start... One. `;
 export const getGameOverMessage = (answer) =>
   `I’m sorry, the correct response was "${answer}". You lose! Thanks for playing Fizz Buzz. For another great Alexa game, check out Song Quiz!`;
-export const getRepromptMessage = (lastNumber) =>
-  `It's your turn. Pick a number, and remember we're playing fizz buzz. My last number was: ${lastNumber}. Please try again, or say 'help' for more information.`;
+export const getGameOverRepromptMessage = `Please say 'new game' to start a new game, or 'stop' to exit.`;
+export const getRepromptMessage = (number) =>
+  `It's your turn. Pick a number, and remember we're playing fizz buzz. My last number was "${number}". Please try again, or say 'help' for more information.`;
 export const getExitMessage = `Thank you for playing fizz buzz!  Let's play again soon!`;
 export const getLaunchMessage = `Welcome to Fizz Buzz. We’ll each take turns counting up from one. However, you must replace numbers divisible by 3 with the word “fizz” and you must replace numbers divisible by 5 with the word “buzz”. If a number is divisible by both 3 and 5, you should instead say “fizz buzz”. If you get one wrong, you lose.`;
-export const getErrorMessage = (lastNumber) =>
-  `I'm sorry - I didn't recognise your answer. My last number was: ${lastNumber}. Please try again, or say 'help' for more information.`;
+export const getErrorMessage = (number) =>
+  `I'm sorry - I didn't recognise your answer. My last number was "${number}". Please try again, or say 'help' for more information.`;
